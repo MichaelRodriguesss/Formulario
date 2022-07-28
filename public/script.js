dfefebe1d39a338dd35c3ab25a4a7e221dbc9dbe
Michael <michaeledavi12@gmail.com>
2022-07-28 16:57:49 -0300

    fisrt commit

---
+++ public/script.js
@@ -0,0 +1,103 @@
+var form = document.getElementById("form");
+var username = document.getElementById("username");
+var telefone = document.getElementById("telefone");
+var cpf = document.getElementById("cpf");
+var email = document.getElementById("email");
+var cep = document.getElementById("cep");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    checkInputs();
+});
+function checkInputs() {
+    var usernameValue = username.value.trim();
+    var emailValue = email.value.trim();
+    var telefoneValue = telefone.value.trim();
+    var cpfValue = cpf.value.trim();
+    var cepValue = cep.value.trim();
+    if (usernameValue === "") {
+        setErrorFor(username, "Preencha esse campo");
+    }
+    else {
+        setSuccessFor(username);
+    }
+    if (telefoneValue === "") {
+        setErrorFor(telefone, "Preencha esse campo");
+    }
+    else if (!isTelefone(telefoneValue)) {
+        setErrorFor(telefone, "Telefone Inválido");
+    }
+    else {
+        setSuccessFor(telefone);
+    }
+    if (cpfValue === "") {
+        setErrorFor(cpf, "Preencha esse campo");
+    }
+    else if (!isCpf(cpfValue)) {
+        setErrorFor(cpf, "CPF Inválido");
+    }
+    else {
+        setSuccessFor(cpf);
+    }
+    if (emailValue === "") {
+        setErrorFor(email, "Preencha esse campo");
+    }
+    else if (!isEmail(emailValue)) {
+        setErrorFor(email, "Email inválido");
+    }
+    else {
+        setSuccessFor(email);
+    }
+    if (cepValue === "") {
+        setErrorFor(cep, "Preencha esse campo");
+    }
+    else if (!isCep(cepValue)) {
+        setErrorFor(cep, "CEP inválido");
+    }
+    else {
+        setSuccessFor(cep);
+    }
+    function setErrorFor(input, message) {
+        var formControl = input.parentElement;
+        var small = formControl.querySelector("small");
+        small.innerText = message;
+        formControl.className = "form-control error";
+    }
+    function setSuccessFor(input) {
+        var formControl = input.parentElement;
+        formControl.className = "form-control success";
+    }
+    function isTelefone(telefone) {
+        return /^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$/.test(telefone);
+    }
+    function isCpf(cpf) {
+        var Soma;
+        var Resto;
+        var cpf = cpf.value;
+        var i = 0;
+        Soma = 0;
+        if (cpf == "00000000000")
+            return false;
+        for (i = 1; i <= 9; i++)
+            Soma = Soma + parseInt(cpfValue.substring(i - 1, i)) * (11 - i);
+        Resto = (Soma * 10) % 11;
+        if (Resto == 10 || Resto == 11)
+            Resto = 0;
+        if (Resto != parseInt(cpfValue.substring(9, 10)))
+            return false;
+        Soma = 0;
+        for (i = 1; i <= 10; i++)
+            Soma = Soma + parseInt(cpfValue.substring(i - 1, i)) * (12 - i);
+        Resto = (Soma * 10) % 11;
+        if (Resto == 10 || Resto == 11)
+            Resto = 0;
+        if (Resto != parseInt(cpfValue.substring(10, 11)))
+            return false;
+        return true;
+    }
+    function isEmail(email) {
+        return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email);
+    }
+    function isCep(cep) {
+        return /^[0-9]{5}-[\d]{3}/.test(cep);
+    }
+}
---
+++ src/script.ts
@@ -0,0 +1,115 @@
+const form = document.getElementById("form") as HTMLInputElement;
+const username = document.getElementById("username") as HTMLInputElement;
+const telefone = document.getElementById("telefone") as HTMLInputElement;
+const cpf = document.getElementById("cpf") as HTMLInputElement;
+const email = document.getElementById("email") as HTMLInputElement;
+const cep = document.getElementById("cep") as HTMLInputElement;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const telefoneValue = telefone.value.trim();
+  const cpfValue = cpf.value.trim();
+  const cepValue = cep.value.trim();
+
+  if (usernameValue === "") {
+    setErrorFor(username, "Preencha esse campo");
+  } else {
+    setSuccessFor(username);
+  }
+
+  if (telefoneValue === "") {
+    setErrorFor(telefone, "Preencha esse campo");
+  } else if (!isTelefone(telefoneValue)) {
+    setErrorFor(telefone, "Telefone Inválido");
+  } else {
+    setSuccessFor(telefone);
+  }
+
+  if (cpfValue === "") {
+    setErrorFor(cpf, "Preencha esse campo");
+  } else if (!isCpf(cpfValue)) {
+    setErrorFor(cpf, "CPF Inválido");
+  } else {
+    setSuccessFor(cpf);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "Preencha esse campo");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "Email inválido");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (cepValue === "") {
+    setErrorFor(cep, "Preencha esse campo");
+  } else if (!isCep(cepValue)) {
+    setErrorFor(cep, "CEP inválido");
+  } else {
+    setSuccessFor(cep);
+  }
+
+  function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+
+    small.innerText = message;
+
+    formControl.className = "form-control error";
+  }
+
+  function setSuccessFor(input) {
+    const formControl = input.parentElement;
+
+    formControl.className = "form-control success";
+  }
+
+  function isTelefone(telefone) {
+    return /^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$/.test(
+      telefone
+    );
+  }
+
+  function isCpf(cpf): boolean {
+    var Soma: number;
+    var Resto: number;
+    var cpf: any = cpf.value;
+    var i: number = 0;
+    Soma = 0;
+
+    if (cpf == "00000000000") return false;
+
+    for (i = 1; i <= 9; i++)
+      Soma = Soma + parseInt(cpfValue.substring(i - 1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+
+    if (Resto == 10 || Resto == 11) Resto = 0;
+    if (Resto != parseInt(cpfValue.substring(9, 10))) return false;
+
+    Soma = 0;
+    for (i = 1; i <= 10; i++)
+      Soma = Soma + parseInt(cpfValue.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if (Resto == 10 || Resto == 11) Resto = 0;
+    if (Resto != parseInt(cpfValue.substring(10, 11))) return false;
+    return true;
+  }
+
+  function isEmail(email) {
+    return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(
+      email
+    );
+  }
+
+  function isCep(cep): boolean {
+    return /^[0-9]{5}-[\d]{3}/.test(cep);
+  }
+}
